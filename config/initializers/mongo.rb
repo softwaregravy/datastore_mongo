b5a941ed5e9c96b738efa158a105603613d56c02
John Hinnegan <john.hinnegan@gmail.com>
2011-11-14 18:34:48 -0800

    fixup the connection string

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,9 @@
 mongo_conn_string = ENV["MONGO_CONNECTION_STRING"]
-mongo_conn_string ||= 'localhost:27017'
+mongo_conn_string ||= 'http://localhost:27017'
 uri = URI.parse(mongo_conn_string)
+puts "connecting to mongo at #{uri.inspect}"
+p uri.host 
+p uri.port
 MongoMapper.connection = Mongo::Connection.new(uri.host, uri.port)
 MongoMapper.database = "#myapp-#{Rails.env}"
 
